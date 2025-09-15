024c21a7c105ea3ef5ff3e991105d7e078ac4f2a
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-15 09:36:32 +0530

    Create Bio_Data.c

---
+++ Bio_Data.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ int main()
+{
+    printf("\n\t------------Bio_Data--------------\n");
+    printf("\n\tName : Rushikesh Mangalkar\n");
+    printf("\n\tEmail : rushikeshmangalkar023@gmail.com\n");
+    printf("\n\t Date of Birth : 17/03/2004\n");
+    printf("\n\tMobile : 9579368461\n");
+    printf("\n\tAddress : Pune\n");
+    printf("\n\tYour Program is Successfully Run\n");
+    printf("\n\t!!!!!!!!!Thank You So Much !!!!!!!!!!!!!\n");
+    
+
+}
+ 
