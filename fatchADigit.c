fd38e71dcd8584819b69e5cbb48a82bf36d3304b
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-24 10:57:13 +0530

    Create fatchADigit.c

---
+++ fatchADigit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+    while(n!=0)
+    {  
+        int digit= n%10;
+        printf("%d\n",digit);
+        n=n/10;
+        /* code */
+    }
+    
+
+    return 0;
+}
