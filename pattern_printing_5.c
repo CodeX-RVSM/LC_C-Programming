455d4fba263326e3a7d7e1dc7c2815cb05ea1479
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-10-03 00:00:21 +0530

    Create pattern_printing_5.c

---
+++ pattern_printing_5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",1);
+        }
+        printf("\n");
+    }
+
+
+}
+// 1
+// 11
+// 111
+// 1111
+// 11111
