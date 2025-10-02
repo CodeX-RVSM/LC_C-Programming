90e344ef26db30fad7a20be9c46f7271df4ac38e
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-10-03 00:02:08 +0530

    Create pattern_printing_06.c

---
+++ pattern_printing_06.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=i;j<=5;j++){
+            printf(j%2!=0?"1":"0");
+        }
+        printf("\n");
+    }
+
+
+}
+
+
+// 10101
+// 0101
+// 101
+// 01
+// 1
