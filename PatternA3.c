d17644356715aac3179a5486886beb3e15416b52
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-10-13 22:45:43 +0530

    Create PatternA3.c

---
+++ PatternA3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    
+     //12345
+    //1    *
+    //2   **
+    //3  ***
+    //4 ****
+    //5*****
+
+    for(int i=1;i<=7;i++){
+        for(int j=6;j>=i;j--){
+            printf(" ");
+        }
+        
+    }
+    //for star 
+    for(int k=1;k<=i;k++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+
+    
