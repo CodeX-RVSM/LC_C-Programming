fffd529fa7bd3e940f26df831aae13fe067d6bfb
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-10-21 17:59:05 +0530

    Create Switch_Odd.c

---
+++ Switch_Odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    //wap to check given no is even or odd
+    int num = 4;//num %2 = 0 odd= 1
+    switch(num%2){
+        case 0:printf("even num");break;
+        case 1:printf("odd num");break;
+    }
+
+    return 0;
+
+}
+//wap to print month days on month number
+//1-31 days,30 days, 28/29 days
