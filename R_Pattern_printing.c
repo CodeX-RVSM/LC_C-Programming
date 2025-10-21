3b40240ab04b1498bec640250e77486a05414c23
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-10-21 17:56:07 +0530

    Create R_Pattern_printing.c

---
+++ R_Pattern_printing.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n=11;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        // if(i==1||j==1||i==n/2+1||(j==n&&i<=n/2+1))   //P
+        if(i==1||j==1||i==n/2+1||(j==n&&i<=n/2+1)||i-j==n/2)
+        // if(j!=n){
+        //     printf("* ");
+        // }else{
+        //     printf("  ");}
+        
+
+        
+        printf("* ");
+        else
+        printf("  ");
+        printf("\n");
+    }    
+    
+    return 0;
+}
