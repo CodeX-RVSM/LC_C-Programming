63cce2b1971440c8c8d3b78b7c50169dfde11a5c
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-11-06 23:35:45 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    //string - collection of characters
+    //end -null termionator \0
+
+    char str[]={'h','e','l','l','o','\0'};
+
+    char str1[]="hello";
+    printf("%s\n",str);
+    printf("%s\n",str1);
+    printf("%c\n",str[3]);
+    
+
+}
