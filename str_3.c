8598a4bec5218814c98d93667dc35914dce0d30b
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-11-11 12:34:30 +0530

    Add mystrcpy function for string copying
    Implement mystrcpy function to copy strings and print them.

---
+++ str_3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+void mystrcpy(char str1[],char str2[]){
+    //logic 
+    char temp=str2;
+    printf("First str :%s\n second str : %s",temp,str2);
+
+}
+
+int main(){
+
+    char str1[]="hello";
+    char str2[]="world";
+    printf("First str: %s\n Second str:%s\n",str1,str2);
+    // strcpy(str1,str2);
+    mystrcpy(str1,str2);
+
+    return 0;
+
+}
